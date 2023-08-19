aed8575b49ee6be2e0f00004e47459c5372abd28
hytric <72875357+hytric@users.noreply.github.com>
2023-08-20 00:16:24 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,37 +1,23 @@
-import cv2
+
 import socket
-import pickle
-import struct
-import time
+import cv2
 
 ip = 'ec2-3-138-105-10.us-east-2.compute.amazonaws.com'
 port = 5001
 
-capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-try:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((ip, port))
+cap = cv2.VideoCapture(0)
 
-        print("connect")
+while True:
+    ret, frame = cap.read()
+    d = frame.flatten()
+    s = d.tostring()
 
-        while True:
-            retval, frame = capture.read()
-            retval, frame = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 90])
+    for i in range(20):
+        sock.sendto(bytes([i]) + s[i*46080:(i+1)*46080], (UDP_IP, UDP_PORT))
 
-            frame = pickle.dumps(frame)
+ 
 
-            print("frame size : {} bytes".format(len(frame)))
-
-            client_socket.sendall(struct.pack(">L", len(frame)) + frame)
-
-            # 전송 속도 제한을 위한 슬립 추가
-            time.sleep(0.03)
-
-except Exception as e:
-    print(f"예외 발생: {e}")
-
-finally:
-    capture.release()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
