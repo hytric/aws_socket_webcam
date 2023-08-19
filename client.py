4f03ccba06158dfe48c58575c79c9cd4bb552dda
hytric <72875357+hytric@users.noreply.github.com>
2023-08-19 23:56:16 +0900

    Update client.py

--- client.py
+++ client.py
@@ -15,7 +15,7 @@ try:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((ip, port))
 
-        print("연결 성공")
+        print("connect")
 
         while True:
             retval, frame = capture.read()
@@ -23,7 +23,7 @@ try:
 
             frame = pickle.dumps(frame)
 
-            print("전송 프레임 크기 : {} bytes".format(len(frame)))
+            print("frame size : {} bytes".format(len(frame)))
 
             client_socket.sendall(struct.pack(">L", len(frame)) + frame)
 
