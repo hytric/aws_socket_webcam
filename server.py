2bb4337bb82cb612c170b1ccc51234a6d38a0db6
hytric <72875357+hytric@users.noreply.github.com>
2023-08-21 22:34:42 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,40 @@
+import socket
+import cv2
+import pickle
+import struct ## new
+
+ip = 'ec2-3-138-105-10.us-east-2.compute.amazonaws.com'
+port = 5001
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
+
+s.bind((ip,port))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+
+conn,addr=s.accept()
+
+data = b""
+payload_size = struct.calcsize(">L")
+print("payload_size: {}".format(payload_size))
+while True:
+    while len(data) < payload_size:
+        print("Recv: {}".format(len(data)))
+        data += conn.recv(4096)
+
+    print("Done Recv: {}".format(len(data)))
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+    print("msg_size: {}".format(msg_size))
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    cv2.imwrite('ImageWindow.png',frame)
+    cv2.waitKey(1)
